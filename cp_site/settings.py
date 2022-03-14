48029a83b339a87ca87d120505dc6c447f28f2c2
Pcrab <pcrab@pcrab.xyz>
2022-03-14 15:24:57 +0800

    feat(main): protect secret
    Put secret key into env file and load through python-dotenv package

--- cp_site/settings.py
+++ cp_site/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import json
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jl#k$@eul^zk(myq8tvz&em)kna!=h#-^%oh#+y2mq0mw4)lq&'
+# SECRET_KEY = 'django-insecure-jl#k$@eul^zk(myq8tvz&em)kna!=h#-^%oh#+y2mq0mw4)lq&'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
