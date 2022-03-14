bc4cf1a85121f01188ef044cd203b5914542c3ed
Pcrab <pcrab@pcrab.xyz>
2022-03-14 09:46:13 +0800

    refactor(urls): add namespace
    Change users path namespace to user
    
    Give user model app_name users

--- cp_site/urls.py
+++ cp_site/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("user/", include("users.urls")),
+    path("user/", include("users.urls", namespace="user")),
     path('admin/', admin.site.urls),
 ]
