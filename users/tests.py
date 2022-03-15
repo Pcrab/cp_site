fa1bb06fe79abdb8aa7d13a7523801fea49b07ef
Pcrab <pcrab@pcrab.xyz>
2022-03-15 18:59:54 +0800

    refactor(users): update test
    Update TestCase file for users to match previous updates. Refactor lots
    of codes to modulize test functions.

--- users/tests.py
+++ users/tests.py
@@ -20,19 +20,18 @@ def gen_random_normal_str(length: int) -> str:
 
 correct_username = gen_random_normal_str(15)
 correct_password = gen_random_normal_str(128)
-correct_salt = gen_random_normal_str(128)
+
+long_username = gen_random_normal_str(21)
+short_username = ""
 
 long_password = gen_random_normal_str(129)
 short_password = gen_random_normal_str(127)
 
-long_salt = gen_random_normal_str(129)
-short_salt = gen_random_normal_str(127)
-
 
 class UserModelTest(TestCase):
 
-    def test_create_user_with_user_already_exist_question(self):
-        pass
+    # def test_create_user_with_user_already_exist_question(self):
+    #     pass
 
     def test_is_valid_with_error_format_question(self):
         """
@@ -42,46 +41,78 @@ class UserModelTest(TestCase):
         2. password length not equal to 128, which should be encrypted by sha3-512 on client side
         3. salt length not equal to 128, for safety
         """
-        username_too_short = User(username="", password=correct_password, salt=correct_salt)
-        self.assertIs(username_too_short.is_valid(), False)
-        username_too_long = User(username=gen_random_normal_str(21), password=correct_password, salt=correct_salt)
-        self.assertIs(username_too_long.is_valid(), False)
 
-        password_too_short = User(username=correct_username, password=short_password, salt=correct_salt)
-        self.assertIs(password_too_short.is_valid(), False)
-        password_too_long = User(username=correct_username, password=long_password, salt=correct_salt)
-        self.assertIs(password_too_long.is_valid(), False)
+        # Correct
+        self.assertEqual(User.is_valid(username=correct_username, unencrypted_password=correct_password), True)
 
-        salt_too_short = User(username=correct_username, password=correct_password, salt=short_salt)
-        self.assertIs(salt_too_short.is_valid(), False)
-        salt_too_long = User(username=correct_username, password=correct_password, salt=long_salt)
-        self.assertIs(salt_too_long.is_valid(), False)
+        # Username
+        self.assertEqual(User.is_valid(username=short_username, unencrypted_password=correct_username), False)
+        self.assertEqual(User.is_valid(username=long_username, unencrypted_password=correct_username), False)
 
-        valid_user = User(username=correct_username, password=correct_password, salt=correct_salt)
-        self.assertIs(valid_user.is_valid(), True)
+        # Password
+        self.assertEqual(User.is_valid(username=correct_username, unencrypted_password=short_password), False)
+        self.assertEqual(User.is_valid(username=correct_username, unencrypted_password=long_password), False)
 
 
 class UserViewTest(TestCase):
+    url_create = reverse("user:create")
+    url_login = reverse("user:login")
+    url_logout = reverse("user:logout")
+    url_destroy = reverse("user:destroy")
+
+    my_username = "pcrab"
+    my_password = "35c68888cbf6fee9c1819187c13fe959aa2a456ddcfc44c7684a980719671b8762239397a33f2e3e77ad3ec0848f67a45fa7ef81681d4f8184a0281d52bc624e"
+
+    def create(self, username: str, password: str) -> int:
+        return self.client.post(self.url_create,
+                                json.dumps({"username": username,
+                                            "password": password}),
+                                "application/json").status_code
+
+    def login(self, username: str, password: str) -> int:
+        return self.client.post(self.url_login,
+                                json.dumps({"username": username,
+                                            "password": password}),
+                                "application/json").status_code
+
+    def logout(self, username: str, password: str) -> int:
+        return self.client.post(self.url_logout).status_code
+
+    def destroy(self, username: str, password: str) -> int:
+        return self.client.post(self.url_destroy,
+                                json.dumps({"username": username,
+                                            "password": password}),
+                                "application/json").status_code
+
+    def setUp(self) -> None:
+        """
+        Create test user for following tests.
+        """
+        print("setup!")
+        self.assertEqual(self.create(self.my_username,
+                                     self.my_password), 200)
+
     def test_create_user_wrong_format_question(self):
         """
         respond code equals BadRequest if format is wrong
         """
-        url = reverse("user:create")
-        response = self.client.post("/user/create/",
-                                    json.dumps({"username": correct_username, "password": short_password}),
-                                    "application/json")
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(self.create(correct_username, short_password), 400)
 
     def test_login_user_error_question(self):
         """
         visit `/user/login/` respond code equals BadRequest(400) if wrong format, or NotFound(404) if user not exist
         """
-        url = reverse("user:login")
-        response = self.client.post(url,
-                                    json.dumps({"username": correct_username, "password": correct_password}),
-                                    content_type="application/json")
-        self.assertEqual(response.status_code, 404)
-        response = self.client.post("/user/login/",
-                                    json.dumps({"username": "pcrab", "password": "asdf"}),
-                                    content_type="application/json")
-        self.assertEqual(response.status_code, 400)
+
+        # Syntax
+        self.assertEqual(self.login(self.my_username, long_password), 400)
+
+        # Wrong Password
+        self.assertEqual(self.login(self.my_username, correct_password), 404)
+
+        # Correct
+        self.assertEqual(self.login(self.my_username,
+                                    self.my_password),
+                         404)
+
+    def test_logout_user_error_question(self):
+        pass
